f85bfccac385bc2b4a7079a22ab8ec1b5f6c6322
saikatnoman12 <noommaa240@gmail.com>
2022-09-30 21:37:35 +0600

    add dom document events handler

--- 2. DOM_Dcoument/app.js
+++ 2. DOM_Dcoument/app.js
@@ -6,19 +6,19 @@
 
 
 
-// use document object method getElementById('isName'). This is return a single html element.
+// use document object method getElementById('isName'). This is return a single html element:---
 const demo1 = document.getElementById('demo1');
 demo1.innerHTML = 'Demo1';
 
 
-// use document object method getElementsByClassName(). This is return a array and multiple html element; 
+// use document object method getElementsByClassName(). This is return a array and multiple html element:---
 const demo2 = document.getElementsByClassName('demo2')
 Array.from(demo2).forEach(function (element) {
     element.style.color = 'red';
 });
 
 
-// use document object method getElementsByTagName('htmlTagName'). This return same html tags in an array.
+// use document object method getElementsByTagName('htmlTagName'). This return same html tags in an array:---
 const allH4 = document.getElementsByTagName('h4');
 for (let element of allH4) {
 
@@ -33,22 +33,22 @@ for (let element of allH4) {
 
 
 
-// use document innerHTML property;
+// use document innerHTML property:---
 const addInnerHtml = document.getElementById('add-text');
 addInnerHtml.innerHTML = 'Hello my good friend';
 
 
-// use document object attribute property;
+// use document object attribute property:---
 const demo3 = document.getElementById('demo3');
 demo3.className = 'h3';
 
 
-// use document style object;
+// use document style object:---
 const h1 = document.getElementsByTagName('h1');
 h1[0].style.background = 'green';
 
 
-// use document method setAttribute('attribute', 'attributeValue');
+// use document method setAttribute('attribute', 'attributeValue'):---
 const h6 = document.getElementsByTagName('h6');
 h6[0].setAttribute('class', 'addClass');
 
@@ -58,17 +58,17 @@ h6[0].setAttribute('class', 'addClass');
 
 
 
-// use document createElement('htmlTagName') method;
+// use document createElement('htmlTagName') method:---
 const createNewElement = document.createElement('h1');
 createNewElement.innerText = 'Create Element.'
 
 
-// use document appendChild('createElementVariableName') method;
+// use document appendChild('createElementVariableName') method:---
 const addElement = document.getElementById('add-element');
 addElement.appendChild(createNewElement);
 
 
-// use document method removeChild() ;
+// use document method removeChild():---
 const parentDiv = document.getElementById('parent-div');
 
 /* -- first type removeChild() document method use -- */
@@ -80,7 +80,7 @@ const removeDivTwo = document.getElementById('remove-child-tow');
 removeDivTwo.parentNode.removeChild(removeDivTwo);
 
 
-// use document method replaceChild()
+// use document method replaceChild():---
 /* -- old element; -- */
 const oldElement = document.getElementById('child-addElement');
 
@@ -101,6 +101,22 @@ const getParentElement = document.getElementById('parent-addElement');
 getParentElement.replaceChild(newElementTwo, oldElement);
 
 
-// use document method write();
+// use document method write():---
 const writeMethod = document.getElementById('writeMethod');
+
+
+
+/* ======== Adding events Handler ======== */
+
+
+
+// use document method onclick() handler:---
+const btn = document.getElementById('button');
+
+const getShowTextDiv = document.getElementById('button-click-show');
+
+btn.onclick = function () {
+    getShowTextDiv.innerText = 'Hay Hay, why are you click my bell btn?';
+}
+
 
