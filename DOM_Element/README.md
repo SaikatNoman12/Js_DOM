5443ad82026afbc225f5fe902b91900ff45d4f88
saikatnoman12 <noommaa240@gmail.com>
2022-10-01 16:13:32 +0600

    add DOM element

---
+++ DOM_Element/README.md
@@ -0,0 +1,39 @@
+# JavaScript DOM element
+
+## DOM element method:
+
+    document.getElementById("idName");
+
+    document.getElementsByClassName("className");
+
+    document.getElementsByTagName("HtmlTag");
+
+    document.querySelector(".class/#id");
+
+    document.querySelectorAll(".className");
+
+## DOM element property:
+
+### The following HTML objects (and object collections) are also accessible:
+
+    document.documentElement;  // get html tag. and html inner tags;
+
+    document.head;      // get html head tag. and head inner tags;
+
+    document.body;      // get html body. and body inner tags;
+
+    document.title;     // get html title tag value.
+
+    document.forms;     // get all html forms. and forms inner tags;
+
+    document.anchors;   // must use name="" attribute in anchor tag. And must set name attribute value.
+
+    document.links;      // get all anchor tags.
+
+    document.embeds;    // get all html embed tag.
+
+    document.images;    // get all html img tag.
+
+    document.scripts;    // get all scripts tag.
+
+
--- DOM_Element/app.js
+++ DOM_Element/app.js
@@ -1,4 +1,4 @@
-/* === DOM element === */
+/* /* === DOM element === */
 
 // use document object method getElementById("idName"):----
 const getDemoId = document.getElementById('demo');
@@ -27,17 +27,39 @@ const getQueryId = document.querySelector('#queryId');
 getQueryId.style.background = 'red';
 
 
+// use document object property documentElement:----
+console.log("-----------------document.documentElement-----------------");
+console.log(document.documentElement);
+
+
+// use document object property head:----
+console.log("-----------------document.head-----------------");
+console.log(document.head);
+
+
+// use document object property body:----
+console.log("-----------------document.body-----------------");
+console.log(document.body);
+
+
+// use document object property title:----
+console.log("-----------------document.title-----------------");
+console.log(document.title);
+
+
+
+// use document object forms property:----
 const getForms = document.forms['myForm'];
 const formTextShow = document.querySelector('.formTextShow');
 let text = "";
 
-// show default;
+/**show default**/
 for (let i = 0; i < getForms.length; i++) {
     text += getForms[i].value + "<br>";
 }
 formTextShow.innerHTML = text;
 
-// show dynamically;
+/**show dynamically**/
 getForms[getForms.length - 1].addEventListener('click', function () {
     text = "";
     for (let i = 0; i < getForms.length; i++) {
@@ -47,22 +69,29 @@ getForms[getForms.length - 1].addEventListener('click', function () {
 });
 
 
+
 // use document object property anchor:----
+console.log("-----------------document.anchors-----------------");;
 const allAnchor = document.anchors;
 document.querySelector('#totalAnchor').textContent = "Total anchor: " + allAnchor.length;
+console.log(allAnchor);
 
 
-// use document object property documentElement:----
-console.log(document.documentElement);
+// use document object property links:---
+console.log("-----------------document.links-----------------");
+console.log(document.links);
 
 
 // use document object property embeds. use for embed html tag:----
+console.log("-----------------document.embeds-----------------");
 console.log(document.embeds);
 
 
-// 
+// use document object images property:----
+console.log("-----------------document.images-----------------");
+console.log(document.images);
 
 
-
-
-
+// use document object property scripts:----
+console.log("-----------------document.scripts-----------------");
+console.log(document.scripts);
